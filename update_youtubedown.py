ad7b06b6d71a3c67d69a20a339cc6002a3ccdf87
Vincent Zee <vincentpeterzee@gmail.com>
2019-12-30 20:03:45 +0100

    Expanded the get_local_version() so that it installs youtubedown.

--- update_youtubedown.py
+++ update_youtubedown.py
@@ -9,6 +9,7 @@ import sys
 import requests
 
 path_to_file: str = "/Users/vincentzee/bin/youtubedown"
+install_dir: str = "/Users/vincentzee/bin"
 
 url: str = "https://jwz.org/hacks/youtubedown"
 
@@ -18,8 +19,20 @@ def get_local_version(path_to_file: str) -> str:
     try:
         f = open(path_to_file, "rt")
     except FileNotFoundError as e:
-        print(e)
-        sys.exit()
+        print("It seems that Youtubedown is not installed on your system.")
+        result = input("Do you want to install it? [y/n] ")
+
+        if result == "y" or result == "yes":
+            print("Installing")
+            s = get_remote_version(url)
+            f = open(path_to_file, "w")
+            print("Writing file")
+            f.write(s)
+            f.close()
+            sys.exit()
+        else:
+            print("Exiting")
+            sys.exit()
 
     for line in f:
         if "$Revision" in line:
@@ -32,6 +45,7 @@ def get_remote_version(url: str) -> str:
     """Gets the remote version and returns it as a long string."""
     response = requests.get(url)
     if response:
+        print("Getting remote version")
         s = response.text
         return s
     else:
@@ -48,6 +62,7 @@ def get_version_number(long_string: str) -> str:
 
 
 def compare_versions(localv: str, remotev: str) -> None:
+    """Compare the version numbers of the remote and local version."""
     if float(localv) < float(remotev):
         print(f"local version {localv} is older than remote version {remotev}")
     else:
@@ -55,11 +70,15 @@ def compare_versions(localv: str, remotev: str) -> None:
 
 
 def main() -> None:
+    # Getting the local version number
+    localv = get_local_version(path_to_file)
+
     remote_version = get_remote_version(url)
 
-    localv = get_local_version(path_to_file)
+    # Getting the remote version number
     remotev = get_version_number(remote_version)
 
+    # Comparing the two versions
     compare_versions(localv, remotev)
 
 

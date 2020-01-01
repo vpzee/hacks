f8a3f6761dbc1af9785718b73dfd9d093aeea2dd
Vincent Zee <vincentpeterzee@gmail.com>
2020-01-01 15:55:26 +0100

    It works to what I want it to do for now.

--- update_youtubedown.py
+++ update_youtubedown.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-
 # update_youtubedown
-
 # This program looks if there is a new version of youtubedown.
 # The original lives at https://www.jwz.org/hacks/youtubedown
 
+import os.path
 import sys
 import requests
 
@@ -14,31 +13,44 @@ install_dir: str = "/Users/vincentzee/bin"
 url: str = "https://jwz.org/hacks/youtubedown"
 
 
+def installed():
+    if os.path.isfile(path_to_file):
+        return True
+    else:
+        return False
+
+
+def install_dir_exists():
+    if os.path.isdir(install_dir):
+        return True
+    else:
+        return False
+
+
+def make_executable(path_to_file):
+    os.chmod(path_to_file, 0o755)
+
+
+def install_it(new_version):
+    """Install 'youtubedown'."""
+    f = open(path_to_file, "w")
+    print("Writing file")
+    f.write(new_version)
+    f.close()
+
+
 def get_local_version(path_to_file: str) -> str:
     """Returns the version number of the local version as a string."""
     try:
         f = open(path_to_file, "rt")
+        for line in f:
+            if "$Revision" in line:
+                l = line.split(" ")
+        f.close()
+        return l[4]
     except FileNotFoundError as e:
-        print("It seems that Youtubedown is not installed on your system.")
-        result = input("Do you want to install it? [y/n] ")
-
-        if result == "y" or result == "yes":
-            print("Installing")
-            s = get_remote_version(url)
-            f = open(path_to_file, "w")
-            print("Writing file")
-            f.write(s)
-            f.close()
-            sys.exit()
-        else:
-            print("Exiting")
-            sys.exit()
-
-    for line in f:
-        if "$Revision" in line:
-            l = line.split(" ")
-    f.close()
-    return l[4]
+        print(e)
+        sys.exit()
 
 
 def get_remote_version(url: str) -> str:
@@ -61,26 +73,63 @@ def get_version_number(long_string: str) -> str:
     return l[4]
 
 
-def compare_versions(localv: str, remotev: str) -> None:
-    """Compare the version numbers of the remote and local version."""
+def is_newer_version(localv: str, remotev: str) -> bool:
+    """Checks if there is a more current version."""
     if float(localv) < float(remotev):
-        print(f"local version {localv} is older than remote version {remotev}")
+        return True
     else:
-        print(f"local version = {localv}; remote version = {remotev}")
+        return False
+
+
+def backup_old_version(localv: str) -> None:
+    new_path: str = path_to_file + "_" + localv
+    print("Old version will be backed up to:")
+    print(new_path)
+    os.rename(path_to_file, new_path)
 
 
 def main() -> None:
-    # Getting the local version number
-    localv = get_local_version(path_to_file)
+    new_version: str = get_remote_version(url)
+    # check if youtubedown is installed
+    if not installed():
+        print("youtubedown doesn't seem to be installed!")
+        answer = input("Do you want to install it? [y/n]\n")
 
-    remote_version = get_remote_version(url)
+        if answer == "y" or answer == "yes":
+            if not install_dir_exists():
+                os.mkdir(install_dir)
+                install_it(new_version)
+                make_executable(path_to_file)
+            else:
+                install_it(new_version)
+                make_executable(path_to_file)
+        else:
+            print("Exititng")
+            sys.exit()
+    elif installed():
+        # getting local and remote version strings
+        localv: str = get_local_version(path_to_file)
+        remotev: str = get_version_number(new_version)
 
-    # Getting the remote version number
-    remotev = get_version_number(remote_version)
+        if is_newer_version(localv, remotev):
+            print("There is a newer version available.")
+            print(f"old version {localv}; new version {remotev}")
+            answer = input("Do you want to install it? [y/n]\n")
+            if answer == "y" or answer == "yes":
+                backup_old_version(localv)
+                install_it(new_version)
+                make_executable(path_to_file)
+            else:
+                print("Exiting")
+                sys.exit()
+        else:
+            print(f"You have the latest version {localv}.")
+    else:
+        print("Something unexpected happened!")
 
-    # Comparing the two versions
-    compare_versions(localv, remotev)
 
+# Comparing the two versions
+#    compare_versions(localv, remotev)
 
 if __name__ == "__main__":
     main()
